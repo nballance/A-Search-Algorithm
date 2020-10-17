27198e1f20384a916743e3f2d8e99671ee7c85d5
nballance <59257894+nballance@users.noreply.github.com>
2020-10-17 12:41:25 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+from Map import *
+
+
+def main():
+    make_grid(120, 160)
+    #passed
+    make_hard()
+    #passed
+    
+    make_highway()
+    #failed
+
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
