55eff9e26edf9a22d759f33f685c6fcf48054d6b
nballance <59257894+nballance@users.noreply.github.com>
2020-10-18 20:26:50 -0400

    Update Heuristic_Search.py

--- Heuristic_Search.py
+++ Heuristic_Search.py
@@ -1,38 +1,58 @@
 from heapq import heapify, heappush, heappop
 from Map import *
 import math
-WEIGHT = 1
+from queue import PriorityQueue
 
+A_WEIGHT = 1     
 
-# def a_search():
+#Uniform Cost Search
+
+# def u_search():
+#     count = 0
+#     cost = 0
+#     visited = []
+#     fringe = PriorityQueue()
 #     current = grid[start[0], start[1]]
-#     open_list = []
-#     closed_list = []
+#     destination = grid[goal[0], goal[1]]
 
-    # heapify(open_list)
-
-    # while (current != goal): 
-    #     neighbors = get_neighbors(current)
-    #     samePriority = []
-    #     length = len(neighbors)
-    #     for i in range(length):
-    #         cur_neighbor = neighbors[i]
-    #         if ((neighbors[i] not in closed_list) and (neighbors[i] not in open_list)):
-    #             calculate_h_val(neighbors[i])
-    #             calculate_g_val(neighbors[i], neighbors[i].parent)
-    #             heappush(open_list, (neighbors[i].f_cost, neighbors[i]))
+#     while (count < 120*160):
+#         neighbors = get_neighbors(current)
+        
+#         for i in range(0, neighbors.length-1):
+#             #set the parent for the neighbors
+#             neighbors[i].parent = current
+#             if(neighbors[i] in fringe):
+#                 fringe = searchFringe
                 
-    #     heapify(open_list)
-                
-#c              
+            
+#             fringe.put(calculate_g_val(neighbors[i]), neighbors[i])
 
-def a_search():
+#     pass
+
+def uniform_search():
+    a_search(0)
+
+# def searchFringe(fringe, cell):
+#     new_fringe = PriorityQueue()
+#     while not fringe.empty():
+#         next_item = fringe.get()
+#         if(next_item.g_cost > calculate_g_val()):
+#             next_item.g_cost =  cell.g_cost
+#             new_fringe.put(cell.g_cost)
+
+
+#A*_Algorithm (weight of 1)
+def a_search_standard():
+    a_search(A_WEIGHT)
+
+
+def a_search(weight):
     open_list = []
     closed_list = []
 
     #Calculate heuristic distance of start vertex to destination (h)
     grid[start[0], start[1]].g_cost = 0
-    grid[start[0], start[1]].h_cost = calculate_h_val(grid[start[0], start[1]])
+    grid[start[0], start[1]].h_cost = calculate_h_val(grid[start[0], start[1]], weight)
 
     #Calculate f value for start vertex (f = g + h, where g = 0)
     grid[start[0], start[1]].f_cost = calculate_f_val(grid[start[0], start[1]])
@@ -42,7 +62,7 @@ def a_search():
     destination = grid[goal[0], goal[1]]
 
     while(current != destination):
-        neighbors = get_neighbors()
+        neighbors = get_neighbors(current)
     # |  FOR Each vertex adjacent to current
         num_neighbors = neighbors.length()
         for i in range(0, num_neighbors-1):
@@ -56,17 +76,28 @@ def a_search():
             # |  |    |    Calculate distance to goal (h)
                 calculate_h_val(neighbors[i])    
             # |  |    |    Calculate f value (f = g + h)  
-                calculate_f_val(neighbors[i])  
+                calculate_f_val(neighbors[i], weight)  
             # |  |    |    Add vertex to open list
-                open_list.append(neighbors[i])
+                heappush(open_list, (neighbors[i].h_cost, neighbors[i].g_cost, neighbors[i]))
             elif(neighbors[i] in open_list):
+                #see if the new f value is less than the previous f value
                 #see if the new g value is less than the previous g value
                 old_parent = neighbors[i].parent
-                neighbors[i].parent = current
+                old_g_val = neighbors[i].g_cost
+                old_f_val = neighbors[i].f_cost
 
-                if(neighbors[i].g_cost <= calculate_g_val(neighbors[i])):
+                neighbors[i].parent = current
+                calculate_g_val(neighbors[i])
+                calculate_f_val(neighbors[i])
+                
+                if(neighbors[i].f_cost <= old_f_val):
+                    if(neighbors[i].g_cost <= old_g_val):
+                        pass
+                    else:
+                        neighbors[i].parent = old_parent
+                else:
                     neighbors[i].parent = old_parent
-        heapify(open_list)
+        
 
     # |  |    |   IF new f value < existing f value or there is no existing f value Then
     # |  |    |    |    Update f value
@@ -78,13 +109,12 @@ def a_search():
     # |  NEXT adjacent vertex
         closed_list.append(current)
     # |  Add current vertex to closed list
-        current = open_list.pop()
-        while(open_list.peak()):
-            next_cell = open_list.peak()
-            if(next_cell.h_cost < current.h_cost):
-                open_list.append(current)
-                current = open_list.pop()
-        heapify(open_list)
+        current = heappop(open_list)
+        while(open_list[0] != None):
+            if(open_list[0].h_cost < current.h_cost):
+                temp = heappop(open_list)
+                heappush(open_list, (current.h_cost, current.g_cost, current))
+                current = temp
 
     # |  Remove vertex to closed list
     # |  Remove vertex with lowest f value from open list and make it current
@@ -92,6 +122,7 @@ def a_search():
 
 
 #Returns the cost from starting node
+#set parent before calculate_g_val is called
 def calculate_g_val(cell):
     terrain1 = cell.terrain
     terrain2 = cell.parent.terrain
@@ -132,9 +163,9 @@ def calculate_g_val(cell):
     
 
 #Cost to ending node
-def calculate_h_val(cell):
+def calculate_h_val(cell, weight):
     distance_to_goal = distance(cell)
-    cell.h_cost = (distance_to_goal[0] * math.sqrt(2) + distance_to_goal[1]) 
+    cell.h_cost = (distance_to_goal[0] * weight * math.sqrt(2) + distance_to_goal[1] * weight) 
     grid[cell[cell.row], cell[cell.col]].h_cost = cell.h_cost
 
 #G Cost + H Cost
@@ -142,10 +173,6 @@ def calculate_h_val(cell):
 def calculate_f_val(cell):
     cell.f_cost = cell.g_cost + cell.h_cost
     grid[cell[cell.row], cell[cell.col]].f_cost = cell.f_cost
-
-#Number of diagonals and number of straight path
-
-#Number of diagonals and number of straight path
 
 #Number of diagonals and number of straight path
 def distance(cell):
