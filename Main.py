35c2f87799b1b2dd73edbfb04be521001cefd75b
nballance <59257894+nballance@users.noreply.github.com>
2020-10-18 20:28:55 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,13 +8,23 @@ def main():
     #print_grid()
     #passed
 
-    #make_hard()
-    #passed
+    # make_hard()
+    # #passed
 
      
-    make_highway()
-    #failed
+    # make_highway()
+    # #passed
 
+    # make_blocked()
+
+    # #read_grid_file("example_grid.txt")
+    # print_grid()
+
+    make_start()
+    make_goal()
+    make_hard()
+    
+    print(write_grid_file())
     print("hello world!")
 
 if __name__ == "__main__":
