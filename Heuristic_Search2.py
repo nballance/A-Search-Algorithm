f0d6e1b179b7e2310b931e0ced8df1138ce171e7
nballance <59257894+nballance@users.noreply.github.com>
2020-10-20 23:48:54 -0400

    Create Heuristic_Search2.py

---
+++ Heuristic_Search2.py
@@ -0,0 +1,283 @@
+from heapq import *
+from Map import *
+import math
+from queue import PriorityQueue
+
+
+
+A_WEIGHT = 1     
+start = (0, 0)
+goal = (6, 3)
+
+
+#Uniform Cost Search
+
+# def u_search():
+#     count = 0
+#     cost = 0
+#     visited = []
+#     fringe = PriorityQueue()
+#     current = grid[start[0], start[1]]
+#     destination = grid[goal[0], goal[1]]
+
+#     while (count < 120*160):
+#         neighbors = get_neighbors(current)
+        
+#         for i in range(0, neighbors.length-1):
+#             #set the parent for the neighbors
+#             neighbors[i].parent = current
+#             if(neighbors[i] in fringe):
+#                 fringe = searchFringe
+                
+            
+#             fringe.put(calculate_g_val(neighbors[i]), neighbors[i])
+
+#     pass
+
+def uniform_search():
+    a_search(0)
+
+# def searchFringe(fringe, cell):
+#     new_fringe = PriorityQueue()
+#     while not fringe.empty():
+#         next_item = fringe.get()
+#         if(next_item.g_cost > calculate_g_val()):
+#             next_item.g_cost =  cell.g_cost
+#             new_fringe.put(cell.g_cost)
+
+
+#A*_Algorithm (weight of 1)
+def a_search_standard():
+    a_search(A_WEIGHT)
+
+
+def a_search(weight):
+    open_list = []
+    closed_list = []
+    
+    # x_start = int(start[0])
+    x_start = 0
+    y_start = 0
+    x_goal = 6
+    y_goal = 3
+    # y_start = int(start[1])
+    # x_goal = goal[0]
+    # y_goal = goal[1]
+
+    #Calculate heuristic distance of start vertex to destination (h)
+    grid[x_start][y_start].g_cost = 0
+    calculate_h_val(x_start, y_start, weight)
+
+    #Calculate f value for start vertex (f = g + h, where g = 0)
+    calculate_f_val(x_start, y_start)
+
+    # |WHILE current vertex is not destination
+    current = grid[x_start][y_start]
+    destination = grid[x_goal][y_goal]
+
+    while(current is not (destination)):
+    # while(current.row != x_goal and current.col != y_goal):
+        neighbors = get_neighbors(current.row, current.col)
+        
+        # print("Current Cell: ", current.row, current.col, x_start, y_start)
+        # for i in range (len(neighbors)):
+        #     print(neighbors[i].terrain)
+        # print("Current Neighbors: ", neighbors)
+
+    # |  FOR Each vertex adjacent to current
+        num_neighbors = len(neighbors)
+        # print("The Number of Neighbors: ", num_neighbors)
+        
+        for i in range (num_neighbors):
+        # |  |    IF vertex not in closed list and not in open List THEN
+            in_closed = False
+            in_open = False
+            for j in range(len(closed_list)):
+
+                if((neighbors[i].row == closed_list[j].row) and (neighbors[i].col == closed_list[j].col)):
+                    in_closed = True
+            for j in range(len(open_list)):
+                if(neighbors[i].row == open_list[j].get('cell').row and (neighbors[i].col == open_list[j].get('cell').col)):
+                    in_open = True
+            # if ((neighbors[i] not in closed_list) and (neighbors[i] not in open_list)):
+            if(not in_open and not in_closed):    
+                neighbors[i].parent = current
+
+            # |  |    |    Calculate distance from start (g)
+                calculate_g_val(neighbors[i].row, neighbors[i].col)
+            # |  |    |    Calculate distance to goal (h)
+                calculate_h_val(neighbors[i].row, neighbors[i].col, weight)    
+            # |  |    |    Calculate f value (f = g + h)  
+                calculate_f_val(neighbors[i].row, neighbors[i].col)  
+            # |  |    |    Add vertex to open list
+                open_list.append( {'cell': neighbors[i], 'f': neighbors[i].f_cost})
+            
+            elif(in_open):
+                #see if the new f value is less than the previous f value
+                #See if the new h val is less than prev
+                old_h_val = neighbors[i].h_cost
+                old_f_val = neighbors[i].f_cost
+
+                calculate_h_val(neighbors[i].row, neighbors[i].col, weight)
+                calculate_g_val(neighbors[i].row, neighbors[i].col)
+                calculate_f_val(neighbors[i].row, neighbors[i].col)
+                
+                if(neighbors[i].f_cost < old_f_val):
+                    neighbors[i].parent = current
+                                        
+                    open_list.append( {'cell': neighbors[i], 'f': neighbors[i].f_cost})
+                elif(neighbors[i].f_cost == old_f_val):
+                    if(neighbors[i].h_cost < old_h_val):
+                        neighbors[i].parent = current
+                        #update the old h value of cell that has the same f value
+                        open_list.append({'cell': neighbors[i], 'f': neighbors[i].f_cost})
+
+        closed_list.append(current)
+  
+        # sorted(open_list, key=mySort)
+        open_list.sort(reverse=True, key=mySort)
+        current  = open_list.pop().get('cell')
+        
+
+    # |  Remove vertex to closed list
+    # |  Remove vertex with lowest f value from open list and make it current
+    # |End WHILE   
+    # print("Parent terrain: ", grid[x_goal][y_goal].parent.terrain, "Parent x: ", grid[x_goal][y_goal].parent.row, "Parent y: ", grid[x_goal][y_goal].parent.col)
+    # print("Parent terrain: ", grid[x_goal][y_goal].parent.parent.terrain, "Parent x: ", grid[x_goal][y_goal].parent.parent.row, "Parent y: ", grid[x_goal][y_goal].parent.parent.col)
+
+    
+    #  grid[x_goal][y_goal]
+    path_coordinates(x_goal, y_goal)
+
+
+def mySort(Cell):
+    return Cell['f']
+
+def path_coordinates(x_goal, y_goal):
+    current = grid[x_goal][y_goal]
+    path = []
+    while(current is not None):
+        path.append((current.row, current.col))
+        print("X of current: ", current.row, "Y of current: ", current.col)
+        
+        current = current.parent
+    return path
+
+#Returns the cost from starting node
+#set parent before calculate_g_val is called
+def calculate_g_val(x, y):
+    terrain1 = grid[x][y].terrain
+    terrain2 = grid[x][y].parent.terrain
+    
+    # print(grid[x][y], grid[x][y].parent)
+    # print("The parent of ", grid[x][y].row, grid[x][y].col, " is ", grid[x][y].parent.row, grid[x][y].parent.col)
+    
+    highway = False
+    diagonal = False
+    cost = 0
+
+    #Diagonal or not
+    if((x != grid[x][y].parent.row) and (y != grid[x][y].parent.col)):
+        diagonal = True
+
+    if((terrain1 == 'a' or terrain1 =='b') and (terrain2 == 'a' or terrain2 =='b')):
+        highway = True
+    
+    #Regular to Regular
+    if((terrain1 == '1' or terrain1 =='a') and (terrain2 == '1' or terrain2 =='a') ):
+        if(diagonal):
+            cost = math.sqrt(2)
+        else:
+            cost = 1 
+
+    #Regular to Hard/Hard to Regular
+    if(((terrain1 == '1' or terrain1 =='a') and (terrain2 == '2' or terrain2 =='b')) or ((terrain1 == '2' or terrain1 =='b') and (terrain2 == '1' or terrain2 =='a'))):
+        if(diagonal):
+            cost = (math.sqrt(2) + math.sqrt(8)) /2
+        else:
+            cost = 1.5 
+    #Hard to Hard
+    if((terrain1 == '2' or terrain1 =='b') and (terrain2 == '2' or terrain2 =='b') ):
+        if(diagonal):
+            cost = math.sqrt(8)
+        else:
+            cost = 2
+    if(highway):
+        cost = cost/4
+
+    grid[x][y].g_cost = grid[x][y].parent.g_cost + cost 
+    
+
+#Cost to ending node
+def calculate_h_val(x, y, weight):
+    distance_to_goal = distance(x, y)
+    # print("Num diagonals: ", distance_to_goal[0], "Num straights: ", distance_to_goal[1])
+    # print(distance_to_goal)
+    grid[x][y].h_cost = (distance_to_goal[0] * weight * math.sqrt(2) + distance_to_goal[1] * weight)
+    # print(grid[x][y].h_cost) 
+    # grid[cell[cell.row], cell[cell.col]].h_cost = cell.h_cost
+
+#G Cost + H Cost
+#Check parent
+def calculate_f_val(x, y):
+    grid[x][y].f_cost = grid[x][y].g_cost + grid[x][y].h_cost
+    # grid[cell[cell.row], cell[cell.col]].f_cost = cell.f_cost
+
+#Number of diagonals and number of straight path
+def distance(x, y):
+    num_diagonals = 0
+    num_straights = 0
+
+    # cell_coordinate = []
+    total_distance = [None] * 2
+
+    # cell_coordinate[0] = cell.col #x val
+    # cell_coordinate[1] = cell.row #y val
+    
+    #Finds the shortest path from the cell parameter to the goal
+    while(not(x == goal[0] and y == goal[1]) ):
+    
+        # print(x, y)
+        
+        #Diagonal up-right
+        if(x < goal[0] and y > goal[1] ):
+            num_diagonals += 1
+            x += 1
+            y -= 1
+        #Diagonal up-left
+        elif(x > goal[0] and y > goal[1] ):
+            num_diagonals += 1
+            x -= 1
+            y -= 1
+        #Diagonal down-left
+        elif(x > goal[0] and y < goal[1] ):
+            num_diagonals += 1
+            x -= 1
+            y += 1
+        #Diagonal down-right
+        elif(x < goal[0] and y < goal[1] ):
+            num_diagonals += 1
+            x += 1
+            y += 1
+
+        #Left
+        elif(x > goal[0] and y == goal[1]):
+            num_straights += 1
+            x -= 1
+        #Right
+        elif(x < goal[0] and y == goal[1]):
+            num_straights += 1
+            x += 1
+        #Down 
+        elif(x == goal[0] and y < goal[1]):
+            num_straights += 1
+            y += 1
+        #Up
+        elif(x == goal[0] and y > goal[1]):
+            num_straights += 1
+            y -= 1
+        
+    total_distance[0] = num_diagonals
+    total_distance[1] = num_straights
+    #return ("Number of diagonals: ", num_diagonals, "Number of straights: ", num_straights)
+    return total_distance
